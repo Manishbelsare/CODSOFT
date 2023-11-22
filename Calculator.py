27f774f7b4786731b59993b8843843045d31c09e
Manish Belsare <145156293+Manishbelsare@users.noreply.github.com>
2023-11-22 09:58:50 +0530

    Calculator.py
    Gui Calculator By using Python Code

--- Calculator.py
+++ Calculator.py
@@ -30,24 +30,24 @@ buttons = [
     '7', '8', '9', '/',
     '4', '5', '6', '*',
     '1', '2', '3', '-',
-    '0', '.', '=', '+'
+    '0', '.', '%', '+'
 ]
 
 # Add buttons to the grid
 row_val = 1
 col_val = 0
 for button in buttons:
-    tk.Button(root, text=button, width=5, height=2, command=lambda b=button: on_click(b)).grid(row=row_val, column=col_val)
+    tk.Button(root, text=button, width=10, height=5, command=lambda b=button: on_click(b)).grid(row=row_val, column=col_val)
     col_val += 1
     if col_val > 3:
         col_val = 0
         row_val += 1
 
 # Clear button
-tk.Button(root, text='C', width=5, height=2, command=clear_entry).grid(row=row_val, column=col_val)
+tk.Button(root, text='C', width=10, height=5, command=clear_entry).grid(row=row_val, column=col_val)
 
 # Equal button
-tk.Button(root, text='=', width=5, height=2, command=calculate).grid(row=row_val, column=col_val + 1)
+tk.Button(root, text='=', width=10, height=5, command=calculate).grid(row=row_val, column=col_val + 1)
 
 # Run the main loop
 root.mainloop()
